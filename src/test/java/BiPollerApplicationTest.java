06540a2668944b6e9416bc627809162b219a2cc1
lshadler <lxs2208@rit.edu>
2017-04-10 16:37:11 -0400

    Initial test of Voter insert and access.

--- src/test/java/BiPollerApplicationTest.java
+++ src/test/java/BiPollerApplicationTest.java
@@ -1,57 +1,203 @@
 import com.bipoller.BiPollerApplication;
+import com.bipoller.BiPollerConfiguration;
+import com.bipoller.database.*;
+import com.bipoller.models.*;
+
+import java.sql.SQLException;
+import java.util.Optional;
 import io.dropwizard.setup.Environment;
 import junit.framework.TestCase;
+import org.h2.jdbc.JdbcSQLException;
 import org.junit.Assert;
 import org.junit.Test;
+import unitedstates.US;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BiPollerApplicationTest extends TestCase {
 
-    protected BiPollerApplication dbManager;
+    protected BiPollerApplication   server;
+    protected BiPollerConfiguration config;
+    protected Environment           env;
+
+    protected Poll       samplePoll;
+    protected PollOption sampleOption1,sampleOption2;
+
+    protected Voter         sampleVoter;
+    protected Voter         sampleRepresentative;
+    protected District      sampleHouseDistrict;
+    protected District      sampleSenateDistrict;
+    protected PollRecord    samplePollRecord;
+
+    protected DistrictDAO       districtDAO;
+    protected VoterDAO          voterDAO;
+    protected PollRecordDAO     pollRecordDAO;
+    protected PollOptionDAO     pollOptionDAO;
+    protected PollDAO           pollDAO;
+    protected AccessTokenDAO    tokenDAO;
 
     @Override
-    public void setUp() {
+    /**
+     * Creates sample data for Testing
+     *
+     * Will Eventually open a database connection.
+     */
+    public void setUp() throws Exception {
 
         // Create instance of BiPoller Application
-        dbManager = new BiPollerApplication();
+        server = new BiPollerApplication();
+
+        // Create configuration class
+        config = new BiPollerConfiguration();
+
+
 
         // Attempt to make a connection to database
-        dbManager.createConnection("","Luke Shadler", "test1234");
-        Assert.assertNotNull(dbManager.getConnection());
+        try {
+            server.createConnection("/home/lshadler/bipoller","lshadler","1234");
+            Assert.assertNotNull(server.getConnection());
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+//
+//        // Run Database
+//        try {
+//            server.run(config, env);
+//        }
+//        catch(Exception e) {
+//            e.printStackTrace(); // I should handle this better
+//        }
 
-        // Run Database
-        //dbManager.run(dbManager.getConnection(),new Environment());
-        //Assert. ...
 
 
-        // Create sample tuples to add to tables
+        // Create Sample Tuples
+        sampleHouseDistrict = new District( (long)1     ,
+                                            1           ,
+                                            US.NEW_YORK ,
+                                            CongressionalBody.HOUSE);
+
+        sampleSenateDistrict = new District(    (long)2                  ,
+                                                2                        ,
+                                                US.NEW_YORK              ,
+                                                CongressionalBody.SENATE );
+
+        sampleVoter = new Voter(    0001                    ,
+                                    "Luke Shadler"          ,
+                                    "pass1234"              ,
+                                    "5851234567"            ,
+                                    "1 Lomb Memorial Drive" ,
+                                    "email-me@rit.edu"      ,
+                                    sampleHouseDistrict     ,
+                                    sampleSenateDistrict    ,
+                                    Optional.empty()        );
+
+        sampleRepresentative = new Voter(   0001                                ,
+                                            "Harlan Haskins"                    ,
+                                            "pass4321"                          ,
+                                            "5857654321"                        ,
+                                            "1 Lomb Memorial Drive"             ,
+                                            "email-me@rit.edu"                  ,
+                                            sampleHouseDistrict                 ,
+                                            sampleSenateDistrict                ,
+                                            Optional.of(sampleSenateDistrict)   );
+
+        sampleOption1 = new PollOption( (long) 1,
+                                        (long) 1,
+                                        "Cats"  );
+        sampleOption2 = new PollOption( (long) 2,
+                                        (long) 1,
+                                        "Dogs"  );
+        List<PollOption> opts = new ArrayList<>();
+        opts.add(sampleOption1);
+        opts.add(sampleOption2);
+
+        samplePoll = new Poll(  (long) 1                ,
+                                sampleRepresentative    ,
+                                sampleSenateDistrict    ,
+                                "Cats or Dogs?"         ,
+                                opts                    );
+
+        assertNotNull(sampleVoter);
+        assertNotNull(sampleRepresentative);
+        assertNotNull(sampleHouseDistrict);
+        assertNotNull(sampleSenateDistrict);
+        assertNotNull(samplePoll);
+
+        SQLUtils.dropEverything(server.getConnection());
+        districtDAO   = new DistrictDAO(server.getConnection());
+        voterDAO      = new VoterDAO(server.getConnection(),districtDAO);
+        pollOptionDAO = new PollOptionDAO(server.getConnection());
+        pollDAO       = new PollDAO(server.getConnection(),pollOptionDAO,voterDAO,districtDAO);
+        pollRecordDAO = new PollRecordDAO(server.getConnection(),pollDAO,pollOptionDAO,voterDAO);
+        tokenDAO      = new AccessTokenDAO(server.getConnection(),voterDAO);
+
+
+
+
+
+
+
+
+
     }
 
     @Override
+    /**
+     * Configured to close the connection with the BiPoller Server
+     */
     public void tearDown() {
 
         // Attempt to close connection
-        dbManager.closeConnection();
-        Assert.assertNull(dbManager.getConnection());
-
+        server.closeConnection();
     }
 
     @Test
     /**
      *  Attempts to add new tuples to tables
      */
-    public void insertIntoTables() {}
+    public void testInsertAndAccessVoters() {
+        try {
 
-    @Test
-    /**
-     *  Clears Table of All Elements
-     */
-    public void clearTable() {}
+            SQLUtils.dropEverything(server.getConnection());                  
+            districtDAO.createTable();                                        
+            voterDAO.createTable();                                           
+            pollDAO.createTable();                                            
+            pollOptionDAO.createTable();                                      
+            pollRecordDAO.createTable();                                      
+            tokenDAO.createTable();                                           
 
-    @Test
-    /**
-     *  Tests user authorization
-     */
-    public void testAuthorization() {}
+
+
+            voterDAO.create("Luke Shadler",
+                    "pass1234",
+                    sampleHouseDistrict,
+                    sampleSenateDistrict,
+                    "5851234567",
+                    "1 Lomb Memorial Drive",
+                    "test123@gmail.com",
+                    Optional.of(sampleSenateDistrict));
+
+            voterDAO.create("Harlan Haskins",
+                            "pass4321",
+                            sampleHouseDistrict,
+                            sampleSenateDistrict,
+                            "5857654321",
+                            "1 Lomb Memorial Drive",
+                            "test321@gmail.com",
+                            Optional.empty());
+
+            List<Voter> voters = voterDAO.all();
+            for(Voter v : voters) {
+                System.out.printf("%s: %s\n",v.getName(),v.getEmail());
+            }
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
