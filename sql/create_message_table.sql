94b72a5817077d0f623a675fa1add9677b850233
lshadler <lxs2208@rit.edu>
2017-05-02 20:26:00 -0400

    Increased message text size.

--- sql/create_message_table.sql
+++ sql/create_message_table.sql
@@ -2,5 +2,5 @@ create table if not exists message
    (id bigint auto_increment primary key,
     foreign key (sender_id) references voter(id),
     foreign key (reciever_id) references voter(id),
-    message_text varchar(255) not null,
+    message_text varchar(1024) not null,
     time_sent timestamp not null);
