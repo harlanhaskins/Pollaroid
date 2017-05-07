222e7934506ffd82aa507ca2151337836a9a0a60
Iconmaster <crazybonesj@yahoo.com>
2017-05-07 19:30:09 -0400

    Added random polls

--- src/main/java/com/pollaroid/dummydata/DummyDataDAO.java
+++ src/main/java/com/pollaroid/dummydata/DummyDataDAO.java
@@ -1,6 +1,7 @@
 package com.pollaroid.dummydata;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.Random;
 
@@ -14,6 +15,10 @@ import com.pollaroid.database.PollRecordDAO;
 import com.pollaroid.database.VoterDAO;
 import com.pollaroid.models.CongressionalBody;
 import com.pollaroid.models.District;
+import com.pollaroid.models.Poll;
+import com.pollaroid.models.PollOption;
+import com.pollaroid.models.PollRecord;
+import com.pollaroid.models.Voter;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -44,7 +49,6 @@ public class DummyDataDAO {
 
     public void generateData() throws SQLException {
         generateDistricts();
-        generateRandomVoters();
     }
 
     void generateDistricts() throws SQLException {
@@ -53,34 +57,74 @@ public class DummyDataDAO {
             for (int i = 0; i < 3; i++) {
                 District house = districtDAO.create(i, state, CongressionalBody.HOUSE);
                 District senate = districtDAO.create(i, state, CongressionalBody.SENATE);
+                
                 generateRepresentatives(house, senate);
+                generateRandomPolls(house); generateRandomPolls(senate);
             }
+            
+            generateRandomVoters(state);
         }
     }
     
-    void generateRandomVoters() throws SQLException {
-        for (US state : US.values()) {
-            System.out.println("Creating voters for " + state.getANSIAbbreviation());
-            for (int i = 0; i < random.nextInt(20); i++) {
-                District house = districtDAO.getByFieldsOrThrow(random.nextInt(3), state, CongressionalBody.HOUSE);
-                District senate = districtDAO.getByFieldsOrThrow(random.nextInt(3), state, CongressionalBody.SENATE);
-                Person person = fairy.person();
-                voterDAO.create(person.fullName(), person.password(), house, senate,
-                        person.telephoneNumber(), person.getAddress().toString(), person.companyEmail(),
-                        Optional.empty());
-            }
+    void generateRandomVoters(US state) throws SQLException {
+        for (int i = 0; i < random.nextInt(20); i++) {
+            District house = districtDAO.getByFieldsOrThrow(random.nextInt(3), state, CongressionalBody.HOUSE);
+            District senate = districtDAO.getByFieldsOrThrow(random.nextInt(3), state, CongressionalBody.SENATE);
+            Person person = fairy.person();
+            voterDAO.create(person.fullName(), person.password(), house, senate,
+                    person.telephoneNumber(), person.getAddress().toString(), person.companyEmail(),
+                    Optional.empty());
         }
     }
     
     void generateRepresentatives(District house, District senate) throws SQLException {
     	Person person1 = fairy.person();
-        voterDAO.create(person1.fullName(), person1.password(), house, senate,
+    	voterDAO.create(person1.fullName(), person1.password(), house, senate,
                 person1.telephoneNumber(), person1.getAddress().toString(), person1.companyEmail(),
                 Optional.of(house));
         
     	Person person2 = fairy.person();
-        voterDAO.create(person2.fullName(), person2.password(), house, senate,
+    	voterDAO.create(person2.fullName(), person2.password(), house, senate,
         		person2.telephoneNumber(), person2.getAddress().toString(), person2.companyEmail(),
                 Optional.of(senate));
+    }
+    
+    void generateRandomPolls(District district) throws SQLException {
+    	ArrayList<String> legalityTopics = new ArrayList<String>() {{
+			add("weed");
+			add("alcohol");
+			add("smoking");
+			add("domestic violence");
+			add("dogs");
+			add("guns");
+			add("everything");
+		}};
+		
+    	ArrayList<String> helpfulnessTopics = new ArrayList<String>() {{
+			add("medical workers");
+			add("the millitary");
+			add("dogs");
+			add("service jobs");
+			add("capitalism");
+			add("small buisnesses");
+		}};
+		
+    	for (int i = 0; i < random.nextInt(6); i++) {
+    		Poll poll = null;
+    		
+    		switch (random.nextInt(2)) {
+    		case 0:
+        		poll = pollDAO.create(districtDAO.getRepresentative(district.getId()).orElse(null), district, "Should " + legalityTopics.get(random.nextInt(legalityTopics.size())) + " be legal?", new ArrayList<String>() {{
+        			add("Yes");
+        			add("No");
+        		}}); break;
+    		case 1:
+        		poll = pollDAO.create(districtDAO.getRepresentative(district.getId()).orElse(null), district, "What is your opinion on " + helpfulnessTopics.get(random.nextInt(helpfulnessTopics.size())) + " in our country?", new ArrayList<String>() {{
+        			add("They are good for this country.");
+        			add("They don't do anything for us.");
+        			add("They actively harm our country.");
+        		}}); break;
+    		}
+    	}
     }
 }
