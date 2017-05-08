68d51c166629c8c211be499a99f935ff6598db4d
Harlan <harlan@harlanhaskins.com>
2017-05-07 21:09:20 -0400

    Fix semantic error

--- sql/get_top_polls.sql
+++ sql/get_top_polls.sql
@@ -3,7 +3,7 @@ select
   count(poll_record.*) as vote_count,
   (select option
    from poll_option
-   where poll_option.id = poll.id
+   where poll_option.poll_id = poll.id
    order by votes desc limit 1) as popular_option
 from poll left join poll_record on poll.id = poll_record.poll_id
 group by poll.id
