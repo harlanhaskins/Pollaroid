53f43b1c25fc8463fb90ca78380fe17f47f8e795
Stuart Olivera <stuart@stuartolivera.com>
2017-05-07 22:00:29 -0400

    Fix vote form interaction

--- frontend/src/forms/helpers.js
+++ frontend/src/forms/helpers.js
@@ -6,4 +6,4 @@ export const INPUT_CLASS = 'form-control';
 export const Label = ({htmlFor, children}) => <label className='col-sm-3 col-md-2 control-label' htmlFor={htmlFor}>{children}</label>;
 export const FormGroup = ({children}) => <div className='form-group'>{children}</div>;
 export const InputWrapper = ({children}) => <div className='col-sm-9 col-md-10'>{children}</div>;
-export const Radio = ({children, model, value}) => <div className='radio disabled'><label><Control.radio model={model} name={model} value={value} />{children}</label></div>;
+export const Radio = ({children, model, value}) => <div className='radio'><label><Control.radio model={model} name={model} value={value} />{children}</label></div>;
